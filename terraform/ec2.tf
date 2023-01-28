73ffa8dc04d300b83cd4666ea5bda51ae92cb17c
andreburto <tuglyraisin@gmail.com>
2023-01-28 12:17:51 -0600

    Save money, use smaller instance.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,7 +5,7 @@ resource "aws_key_pair" "deployer" {
 resource "aws_instance" "yview_instance" {
     ami             = data.aws_ami.amznlnx2.id
     associate_public_ip_address = true
-    instance_type   = "t3.small"
+    instance_type   = "t3.micro"
     key_name        = aws_key_pair.deployer.key_name
     subnet_id       = data.aws_subnet.selected.id
     security_groups = [aws_security_group.yview_sg.id]
